345842b4953b9c21c3952087538ada7810c16eb1
Aditya Vk <adityakulkarnikv1@gmail.com>
2020-03-04 22:00:27 +0530

    Changed logo

---
+++ app/src/main/java/com/example/BrainBox/Learn_Level_One_Quiz.java
@@ -0,0 +1,13 @@
+package com.example.BrainBox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Learn_Level_One_Quiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn__level__one__quiz);
+    }
+}
